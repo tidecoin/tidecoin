680fc67c8d5856cf9e048b18d16aec4eb78c5a92
Yar Sawyer <idquantum@gmail.com>
2026-03-12 01:54:59 +0300

    fix compiler warnings

--- src/util/strencodings.h
+++ src/util/strencodings.h
@@ -117,7 +117,7 @@ template <typename T>
 T LocaleIndependentAtoi(std::string_view str)
 {
     static_assert(std::is_integral_v<T>);
-    T result;
+    T result{};
     // Emulate atoi(...) handling of white space and leading +/-.
     std::string_view s = util::TrimStringView(str);
     if (!s.empty() && s[0] == '+') {
@@ -178,7 +178,7 @@ template <typename T>
 std::optional<T> ToIntegral(std::string_view str)
 {
     static_assert(std::is_integral_v<T>);
-    T result;
+    T result{};
     const auto [first_nonmatching, error_condition] = std::from_chars(str.data(), str.data() + str.size(), result);
     if (first_nonmatching != str.data() + str.size() || error_condition != std::errc{}) {
         return std::nullopt;
